a32e1a00498b63714c2066650a24528b8eb8acd8
Lucas Seixas <lucasseixas29@hotmail.com>
2023-09-12 18:14:34 -0300

    Aula de conexão nodejs-postgressql finalizada.

---
+++ Md-03/conexao-nodejs-com-postgresSql/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const pool = require('./conexao');
+const { param } = require('express/lib/request');
+
+
+const app = express();
+
+
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    const { pagina, porPagina } = req.query
+    try {
+        // const query = 'select * from empresas where nome = $1 or nome = $2';
+        // const params = ['Google', 'Facebook'];
+        // const query = 'update empresas set site = $1 where id = $2';
+        // const params = ['www.cakewalk.com', 1];
+
+        // const query = `
+        // select e.id as id_empresa, f.id as id_filial, e.nome, f.pais, p.nome as funcionarios 
+        // from empresas e 
+        // join filiais f on e.id = f.empresa_id
+        // join pessoas p on e.id = p.empresa_id;  
+        // `
+
+        // const query = `
+        // select e.id as empreasID, f.id as filialID, e.nome, f.pais from empresas e left join filiais f on e.id = f.empresa_id;
+        // `
+
+        // const query = `
+        // select e.id as empreasID, f.id as filialID, e.nome, f.pais from empresas e left join filiais f on e.id = f.empresa_id;
+        // `
+
+        const query = `select * from pessoas order by id asc limit $1 offset $2;`
+
+        const { rowCount } = await pool.query('select * from pessoas')
+
+        const offset = pagina === 1 ? 0 : (pagina - 1) * porPagina;
+
+        const params = [porPagina, offset];
+
+        const resultado = await pool.query(query, params);
+        const result = {
+            pagina,
+            porPagina,
+            total: rowCount,
+            registros: resultado.rows
+        }
+
+        return res.json(result);
+    } catch (error) {
+        console.log(error.message)
+    }
+
+});
+
+app.listen(3000);
